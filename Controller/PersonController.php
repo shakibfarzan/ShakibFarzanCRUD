ade43f1b7802e335ac6f20db0fb8c9b36f2c1706
Shakib Farzan <farzan.shakib@gmail.com>
2022-01-06 12:58:25 +0330

    complete person controller

--- Controller/PersonController.php
+++ Controller/PersonController.php
@@ -4,6 +4,7 @@ namespace CRUD\Controller;
 
 use CRUD\Model\Actions;
 use CRUD\Model\Person;
+use CRUD\Helper\PersonHelper;
 
 class PersonController
 {
@@ -32,23 +33,48 @@ class PersonController
 
     public function createAction($request)
     {
+        $date = date_create();
         $person = new Person();
-        $var = json_decode(file_get_contents('php://input'), true)['firstName'];
-        echo $var;
+        $person->setId(date_timestamp_get($date));
+        $person->setFirstName($request['firstName']);
+        $person->setLastName($request['lastName']);
+        $person->setUsername($request['username']);
+        if (PersonHelper::getInstance()->insert($person)) {
+            http_response_code(200);
+        } else {
+            http_response_code(400);
+        }
     }
 
     public function updateAction($request)
     {
+        $person = new Person();
+        $person->setId($request['id']);
+        $person->setFirstName($request['firstName']);
+        $person->setLastName($request['lastName']);
+        $person->setUsername($request['username']);
+        if (PersonHelper::getInstance()->update($person)) {
+            http_response_code(200);
+        } else {
+            http_response_code(404);
+        }
     }
 
     public function readAction($request)
     {
+        PersonHelper::getInstance()->fetch($request['id']);
     }
     public function readAllAction($request)
     {
+        PersonHelper::getInstance()->fetchAll();
     }
 
     public function deleteAction($request)
     {
+        if (PersonHelper::getInstance()->delete($request['id'])) {
+            http_response_code(200);
+        } else {
+            http_response_code(404);
+        }
     }
 }
