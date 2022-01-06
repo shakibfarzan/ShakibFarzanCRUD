4e111b0c581e3c086024791e7ba230c2c438c4d3
Shakib Farzan <farzan.shakib@gmail.com>
2022-01-06 09:24:31 +0330

    create connection to DB with DBConnector

---
+++ Helper/DBConnector.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace CRUD\Helper;
+
+use mysqli;
+
+class DBConnector
+{
+
+    /** @var mixed $db */
+    private $db;
+    private string $servername;
+    private string $username;
+    private string $password;
+    private int $port;
+    private $conn;
+
+    public function __construct($servername, $username, $password, $db, $port)
+    {
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->db = $db;
+    }
+
+    /**
+     * @throws \Exception
+     * @return void
+     */
+    public function connect(): void
+    {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->db);
+        if (!$this->conn) {
+            $this->exceptionHandler("Connection failed");
+        } else {
+            echo "Connected successfully";
+        }
+    }
+
+    /**
+     * @param string $query
+     * @return bool
+     */
+    public function execQuery(string $query): bool
+    {
+        if ($this->conn->query($query) === TRUE) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $message
+     * @throws \Exception
+     * @return void
+     */
+    private function exceptionHandler(string $message): void
+    {
+        echo $message;
+    }
+}
